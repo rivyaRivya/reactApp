0f35a3deb3f5d51eb37b97ee91dba7f74114bf00
RIVYA T T <rivyameenu@gmail.com>
2025-03-13 21:18:58 +0530

    pud

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -8,7 +8,7 @@
 
 const fs = require("fs");
 const path = require("path");
-const readline = require("readline");
+const readline = require("readline"); 
 
 const root = process.cwd();
 const oldDirs = ["app", "components", "hooks", "constants", "scripts"];
